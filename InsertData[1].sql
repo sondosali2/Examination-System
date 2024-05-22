f22da58fe252a82242646ca4c1c227bb396e44a2
sondosali2 <142232817+sondosali2@users.noreply.github.com>
2024-05-22 12:13:58 +0300

    Add files via upload

---
+++ InsertData[1].sql
@@ -0,0 +1,171 @@
+use ExaminationSystem
+INSERT INTO Branch VALUES(1,'cairo', 'nasr city');
+INSERT INTO Branch VALUES(2,'giza', 'dokki');
+INSERT INTO Branch VALUES(3,'fayoum', 'dallah');
+INSERT INTO Branch VALUES(4,'alex', 'miami');
+INSERT INTO Branch VALUES(5,'qena', 'qena');
+INSERT INTO Branch VALUES(6,'cairo', '10th of ramadan');
+INSERT INTO Branch VALUES(7,'cairo', 'giza');
+
+INSERT INTO Department VALUES(1, 'marketing');
+INSERT INTO Department VALUES(2, 'finance');
+INSERT INTO Department VALUES(3, 'HR');
+INSERT INTO Department VALUES(4, 'teaching');
+
+INSERT INTO Track VALUES (1, 'web development')
+INSERT INTO Track VALUES (2, 'data science')
+INSERT INTO Track VALUES (3, 'full stack')
+INSERT INTO Track VALUES (4, 'front end')
+INSERT INTO Track VALUES (6, 'Back End')
+ INSERT INTO Track VALUES   (7, 'Algorithmic Analysis')
+ INSERT INTO Track VALUES   (8, 'Computer Networks')
+   INSERT INTO Track VALUES (9, 'Database Systems')
+ select*from track
+
+INSERT INTO Student VALUES('ahmed','ali','05-14-2001', '01096915957', 'aaa@gmail.com','qwerta','asdfasd54f54',3, 1);
+INSERT INTO Student VALUES('ayman','hesham','12-07-1996', '01198277775', 'lpmojkn@yahoo.com','edcrfv','dfhdhj87',1, 1);
+INSERT INTO Student VALUES('hend','ayman','11-18-2004', '01001255455', 'aasdffh@yahoo.com','sfhsdh654','dfhdf87h8',3, 1);
+INSERT INTO Student VALUES('hoda','riad','12-14-2004', '01096915777', 'asdfffdg@gmail.com','sdfgsdfbh','4ndf7hjdf7gh',1, 1);
+INSERT INTO Student VALUES('hedaya','khalied','10-14-2001', '01196948757','sfdgsdfgsdfgh@gmail.com','qwerta','dbdgfdf87h',1, 1);
+INSERT INTO Student VALUES('hader','mahmoud','08-14-2003', '01295555957', 'xcvbn@gmail.com','qwerta','dfhjjhfgfgj8',2, 1);
+INSERT INTO Student VALUES('ahmed','ali','09-14-2003', '01294477957', 'xmxcvbn@gmail.com','qwerta','sdhhdhjdf',3, 2);
+INSERT INTO Student VALUES('hossam','hassan','11-14-2002', '01196914444', 'poeiuty@gmail.com','lskjghsdgh','bdfghdfhdf56',3, 1);
+INSERT INTO Student VALUES('radwa','sherbeny','12-14-2001', '01096915957', 'sdfghsdf@yahoo.com','hjgasdf','sdghh6sdfh5',3, 2);
+INSERT INTO Student VALUES('mohamed','ramadan','05-14-2001', '01012121257', 'rtghgf@yahoo.com','asdfdfh','dfhgfhjdfh2',3, 2);
+INSERT INTO Student VALUES('adel','emam','05-14-1999', '01096159847', 'deghgf@gmail.com','kiolpoytr','asdfasd54f54',2, 2);
+INSERT INTO Student VALUES('hassan','hosny','05-30-2001', '01096333337', 'tuekmrgm,@gmail.com','frdedsrf','sdghh2sdgd5',2, 2);
+INSERT INTO Student VALUES('ahmed','zaki','01-25-1988', '01096222227', 'ryjehymjefh@gmail.com','poityugju','sdghsdf5g44',4, 2);
+INSERT INTO Student VALUES('ahmed','helmy','06-30-2001', '01099999957', 'dsfsdfg@gmail.com','mjnhbgv','sdfgsdfgsdf85',4, 1);
+INSERT INTO Student VALUES('mervet','ameen','01-08-1998', '01299915957', 'sdsdhfg@gmail.com','rywtutuku','vbcvbdbgfh2',3, 1);
+INSERT INTO Student VALUES('amr','diab','05-17-2001', '01596555957', 'aswgdfh@yahoo.com','rjmrtkuket','dfnh6dfgn2d6f1n',1, 1);
+INSERT INTO Student VALUES('somaia','khashab','05-22-2001', '01596111957', 'sdfglksdfjg@gmail.com','gnswhmrymrn','dfhgfhdf6h54',1, 1);
+INSERT INTO Student VALUES('abdelhalem','hafez','01-11-1998', '01596777957', 'sfhddfg@yahoo.com','nghnmsfhm','dghbghsdf3g1',2, 1);
+INSERT INTO Student VALUES('sayed','ragab','01-14-1999', '01191111957', 'gmdfn@gmail.com','bkgbwksdfhs','df3dfgsdfg5s',1, 1);
+INSERT INTO Student VALUES('hesham','abbas','03-14-2001', '01222222957', 'dvngfn@gmail.com','bjwgonuwnju','dfhhsdf3',4, 2);
+INSERT INTO Student VALUES('ahmed','fahmy','02-14-2001', '01214515957', 'sdghgr@gmail.com','bbjkfuigh','32d1fghdfhjdf3g',1, 2);
+INSERT INTO Student VALUES('mostafa','saad','04-14-2002', '01096914526', 'sdgnsgn@gmail.com','wrgjuqhebjqb','dfhsdghh5',2, 2);
+INSERT INTO Student VALUES('mahmoud','kahraba','03-14-2002', '01096910000', 'sdgnhgfn@gmail.com','gfrqetn','sghjwgthjdgfj',2, 2);
+INSERT INTO Student VALUES('omar','marmoush','02-14-2003', '01000005957', 'sfnhdf@gmail.com','jsdfvkjv','654gdhfhjdf',3, 1);
+INSERT INTO Student VALUES('mohamed','samy','03-14-2005', '01094444957', 'finvv@gmail.com','erogijwbkij','dghsdgsdf5621',3, 1);
+INSERT INTO Student VALUES('hamada','ezzo','02-14-1999', '01096955557', 'aaasdhgfh@gmail.com','sfmkbcbjdfh','hwnh321fdg',1, 1);
+INSERT INTO Student VALUES('ayman','elfeel','02-14-1997', '01596995957', 'bbb@yahoo.com','lfobgjgh','hrjfgdh654',4, 1);
+INSERT INTO Student VALUES('mohamed','fikery','01-14-1997', '01116915957', 'asdfbhgfn@gmail.com','igtovmkvbfh','dbsdbsdb3215',2, 2);
+INSERT INTO Student VALUES('yousef','hany','01-14-1998', '01098521487', 'bhdndf@gmail.com','bgldbjsddf','sdfghfsdg987',4, 2);
+INSERT INTO Student VALUES('khalied','khaloda','11-14-2003', '01012515957', 'nnhjdfd@gmail.com','swvglfbibm','weryggd988',4, 1);
+INSERT INTO Student VALUES('shimaa','saber','06-14-2002', '01096915957', 'ffndnm@yahoo.com','ghqetognmj','dfsdfgfh9875',1, 1);
+INSERT INTO Student VALUES('sara','saso','05-14-2002', '01096914857', 'gherjrhmj@gmail.com','ethiopubqji','sg4ghs5f54',1, 1);
+INSERT INTO Student VALUES('shosho','shamy','11-14-2001', '01111115957', 'sdbdfhh@gmail.com','hofhfjbhdfb','fnfhmh24f54',2, 1);
+INSERT INTO Student VALUES('khokho','khalied','12-14-1989', '01116915957', 'sgdhdfh@gmail.com','ehetnrnmn','gsdgbnsgn84',3, 1);
+INSERT INTO Student VALUES('nono','nabil','10-14-2005', '01196915957', 'sdfsdh@gmail.com','aghetheg','thbngb2138',3, 1);
+INSERT INTO Student VALUES('soso','samy','11-14-2004', '01597715957', 'dhdio@yahoo.com','sethbwrj','eryhjehj5458',3, 1);
+INSERT INTO Student VALUES('skeena','shakoush','05-14-1998', '01096948967', 'sdfgosdfgh@gmail.com','qwertgfhdtrha','gsgfnsgfn54547',3, 2);
+INSERT INTO Student VALUES('seif','khangar','09-14-1999', '01096917777', 'shdfh@gmail.com','wfwgfbfb','sdfgsdfg65456',3, 1);
+INSERT INTO Student VALUES('mohamed','farag','04-05-2004', '01199915957', 'sefggh@yahoo.com','knhb1879','nhskldfjg565456',3, 1);
+INSERT INTO Student VALUES('noha','alaa','07-14-2002', '01024815957', 'noooha@yahoo.com','pdhjdhd','sdfsdfgqw987',3, 1)
+
+INSERT INTO Course VALUES(1, 'database', 20, 75, 150);
+INSERT INTO Course VALUES(2, 'operating systems', 30, 75, 150);
+INSERT INTO Course VALUES(3, 'C#', 20, 75, 150);
+INSERT INTO Course VALUES(4, 'OOP', 15, 75, 150);
+INSERT INTO Course VALUES(5, 'data structers', 20, 75, 150);
+INSERT INTO Course VALUES(6, 'ORM', 20, 75, 150);
+INSERT INTO Course VALUES(7, 'ASP.NET MVC', 10, 75, 150);
+INSERT INTO Course VALUES(8, 'ASP.NET Web API', 10, 75, 150);
+INSERT INTO Course VALUES(9, 'HTML', 2, 75, 150);
+INSERT INTO Course VALUES(10, 'CSS', 3, 75, 150);
+INSERT INTO Course VALUES(11, 'photoshop', 12, 75, 150);
+INSERT INTO Course VALUES(12, 'C Programming', 20, 75, 150);
+INSERT INTO Course VALUES(13, 'Microcontroller', 10, 75, 150);
+INSERT INTO Course VALUES(14, 'algorithmic analysis', 20, 75, 150);
+INSERT INTO Course VALUES(16, 'web development', 20, 75, 150);
+INSERT INTO Course VALUES(17, 'database systems', 20, 75, 150);
+
+
+INSERT INTO TrackCourses VALUES (1, 2)
+INSERT INTO TrackCourses VALUES (2, 3)
+INSERT INTO TrackCourses VALUES (3, 4)
+INSERT INTO TrackCourses VALUES (4, 5)
+INSERT INTO TrackCourses VALUES (5, 6)
+INSERT INTO TrackCourses VALUES (6, 7)
+INSERT INTO TrackCourses VALUES (7, 8)
+INSERT INTO TrackCourses VALUES (8, 9)
+INSERT INTO TrackCourses VALUES (9, 10)
+INSERT INTO TrackCourses VALUES (10, 14);
+INSERT INTO TrackCourses VALUES (11, 15);
+INSERT INTO TrackCourses VALUES (12, 16);
+INSERT into TrackCourses VALUES (13, 17);
+select * from TrackCourses
+INSERT INTO Employee VALUES (1, 'Ahmed', 20890.45, '01010832741', 'ahm@gmail.com', 'sdfgsdfhh', 'sdfghh5051', '10-10-2022', 4, 1);
+INSERT INTO Employee VALUES (2, 'Walid', 12650.50, '01163487159', 'WWW@yahoo.com', 'asgafghas', 'sdfhsdf545', '05-05-2021', 2, 1);
+INSERT INTO Employee VALUES (3, 'Bassem', 16548.14, '01587454899', 'ahm@gmail.com', 'sdghjsdh', 'ssghsgh654', '01-01-2019', 1, 2);
+INSERT INTO Employee VALUES (4, 'Hoda', 8954.66, '01212125481', 'hoo@gmail.com', 'sdhsdfgs', 'ssdfhsd548', '02-02-2020', 3, 1);
+INSERT INTO Employee VALUES (5, 'Merna', 25361.51, '01010101010', 'mer@gmail.com', 'eywethgsdfgh', 'adfgfdg45', '12-07-2023', 4, 2);
+INSERT INTO Employee VALUES (6, 'Sondos', 25361.51, '01212121212', 'son@gmail.com', 'dheehh', 'dghsdh54', '12-05-2023', 4, 3);
+INSERT INTO Employee VALUES (7, 'Mohamed', 25000.00, '01012345678', 'mohamed@gmail.com', 'qwertyuiop', 'asdfghjkl654321', '01-01-2020', 4, 2);
+select * from Employee
+INSERT INTO QustionTypes VALUES(1, 'text')
+INSERT INTO QustionTypes VALUES(2, 'TorF')
+INSERT INTO QustionTypes VALUES(3, 'choices')
+INSERT INTO QustionTypes VALUES(4, 'image');
+INSERT INTO QustionTypes VALUES(5, 'video');
+select * from QuestionType
+INSERT INTO Question (Qid, question) VALUES(16, 'Who is the best player?')
+INSERT INTO Question (QId, question) VALUES(17, 'how many ballon dor messi have?')
+INSERT INTO Question (QId, question) VALUES(18, 'Messi is the best player? TorF')
+INSERT INTO Question (QId, question) VALUES(19, 'What is the number after 1? a- 2,  b- 3,  c- 4, d- a & b')
+INSERT INTO Question (QId, question) VALUES(20, 'What is the number after 2? a- 3,  b- 4,  c- 5, d- a & b')
+INSERT INTO Question (QId, question) VALUES(21, 'What is the most popular programming language?')
+INSERT INTO Question (QId, question) VALUES(22, 'What is the most widely used operating system?')
+select * from question
+
+
+INSERT INTO BranchDepartments VALUES (1, 2)
+INSERT INTO BranchDepartments VALUES (1, 4)
+INSERT INTO BranchDepartments VALUES (2, 6);
+INSERT INTO BranchDepartments VALUES (3, 4);
+INSERT INTO BranchDepartments VALUES (4, 8);
+
+select * from BranchDepartments
+select * from Branch
+select * from department
+INSERT INTO BranchTracks VALUES (1,1)
+INSERT INTO BranchTracks VALUES (1,2)
+INSERT INTO BranchTracks VALUES (2, 1);
+INSERT into BranchTracks VALUES (3, 2);
+select * from BranchTracks
+INSERT INTO Exam VALUES (17, '08:00:00', '12:00:00', 4, 2);
+INSERT INTO Exam VALUES (20, '09:00:00', '01:00:00', 2, 3);
+INSERT INTO Exam VALUES (23, '10:00 AM', '2:00', 3, 4);
+INSERT INTO Exam VALUES (24, '10:00 AM', '1:00', 4, 1);
+INSERT INTO Exam VALUES (5, '11:00 AM', '2:00', 5, 2);
+INSERT INTO Exam VALUES (6, '9:30 AM', '12:30', 6, 3);
+INSERT INTO Exam VALUES (7, '10:30 AM', '1:30', 7, 4);
+INSERT INTO Exam VALUES (8, '12:00 PM', '3:00', 8, 1);
+INSERT INTO Exam VALUES (9, '05:00:00', '12:00:00', 10, 2);
+INSERT INTO Exam VALUES (10, '2:00 PM', '4:00', 10, 3);
+INSERT INTO Exam VALUES (11, '3:00 PM', '5:00', 20, 4);
+INSERT INTO Exam VALUES (12, '4:00 PM', '6:00', 30, 1);
+INSERT INTO Exam VALUES (13, '5:00 PM', '7:00', 40, 2);
+INSERT INTO Exam VALUES (14, '6:00 PM', '8:00', 50, 3);
+INSERT INTO Exam VALUES (15, '7:00 PM', '9:00', 60, 4);
+select * from exam
+select * from branch
+select * from course
+INSERT INTO InstructorQestionExam VALUES(1, 1, 1, 10)
+INSERT INTO InstructorQestionExam VALUES(1, 2, 1, 10)
+INSERT INTO InstructorQestionExam VALUES(1, 3, 1, 10)
+INSERT INTO InstructorQestionExam VALUES(1, 4, 1, 10)
+INSERT INTO InstructorQestionExam VALUES(1, 5, 1, 10)
+INSERT INTO InstructorQestionExam VALUES(2, 5, 2, 10);
+INSERT into InstructorQestionExam VALUES(3, 6, 3, 10);
+select * from InstructorQestionExam
+INSERT INTO StudentAnswerExamQestions VALUES(1, 'messi', 1, 1)
+INSERT INTO StudentAnswerExamQestions VALUES(1, '8', 2, 1)
+INSERT INTO StudentAnswerExamQestions VALUES(1, 'F', 3, 1)
+INSERT INTO StudentAnswerExamQestions VALUES(1, 'a', 4, 1)
+INSERT INTO StudentAnswerExamQestions VALUES(1, 'a', 5, 1)
+INSERT INTO StudentAnswerExamQestions VALUES(2, 'x', 5, 2);
+INSERT into StudentAnswerExamQestions VALUES(3, '5', 6, 3);
+select * from StudentAnswerExamQestions
+
+
---
+++ SQLQuery2.sql
@@ -0,0 +1,110 @@
+--this proc returns number of q and takes examid
+CREATE proc sp_get_number_of_qestions_in_exam_by_examid 
+@examid int
+as
+begin
+	select COUNT([QuestionId]) AS 'Number of qestions'
+	from InstructorQestionExam
+	WHERE [ExamId] = @examid
+end
+--this proc get total degree 
+CREATE PROC sp_get_total_degree_of_student_by_studentID_And_ExamID 
+@StudentID INT,
+@ExamID INT
+AS
+BEGIN
+	SELECT SEAQ.[StudID], SEAQ.[ExID], SEAQ.[StudentAnswer], QA.[answer]
+	FROM StudentAnswerExamQustions SEAQ INNER JOIN question Q
+	ON SEAQ.QesID = Q.QId INNER JOIN QuestionAnswer QA
+	ON Q.QId = QA.questionId
+	WHERE SEAQ.StudentAnswer = QA.answer AND SEAQ.[StudID] = @StudentID AND SEAQ.[ExID] = @ExamID
+	SELECT @@ROWCOUNT * 10 AS 'Student Degree', (select SUM([degree]) from[dbo].[InstructorQestionExam] WHERE [ExamId] = @ExamID) AS 'Course Degree'
+END
+--this proc return available tracks and takes branch id
+CREATE PROC SP_GetAvilableTracksInBranchByBranchID 
+@BranchID INT 
+AS
+BEGIN
+	SELECT b.id, b.name AS 'Branch Name', t.name AS 'Track'
+	FROM branch b INNER JOIN trackBranch tb
+	ON b.id = tb.BranchId INNER JOIN track t
+	ON tb.TrackId = t.id
+	WHERE b.id = @BranchID
+END
+--this proc return students name by branch id 
+CREATE PROC SP_GetStudentNameAndBranchByID 
+@ID INT
+AS 
+BEGIN
+	SELECT CONCAT(S.FirstName,' ',S.LastName) AS 'Student Name', b.name AS 'Branch Name'
+	FROM student s INNER JOIN branch b
+	ON s.BranchID = b.id
+	WHERE s.id = @ID
+END
+--this proc takes student,exam,question id and returns question type
+select * from student,exam,question
+CREATE PROCEDURE sp_GetQuestionTypee 
+    @student_id INT,
+    @exam_id INT,
+    @question_id INT
+AS
+BEGIN
+    SELECT qt.questionType
+    FROM Question q
+    INNER JOIN QuestionType qt ON q.QId= qt.questionId
+    WHERE q.QId = @question_id;
+END
+
+--views
+create view branchView
+as
+select * from branch
+
+CREATE VIEW ChoicesQesions
+AS
+SELECT Q.question
+FROM Question Q INNER JOIN QustionTypes QT
+ON Q.QId= QT.id
+WHERE QT.id = 3;
+
+CREATE VIEW TextQesions
+AS
+SELECT Q.question
+FROM Question Q INNER JOIN QustionTypes QT
+ON Q.QId= QT.id
+WHERE QT.id = 1;
+
+create view StudentDegreeView
+as
+select degree from InstructorQestionExam
+
+CREATE VIEW TrueAndFalaseQesions
+AS
+SELECT Q.question
+FROM Question Q INNER JOIN QustionTypes QT
+ON Q.QId = QT.id
+WHERE QT.id = 6 ;
+select * from QuestionType
+
+create function GetStudentWithCity (@city varchar(50))
+returns table
+as
+return(
+	select student.FirstName , student.LastName 
+	from student
+	where student.address = @city
+) 
+SELECT * FROM GetStudentWithCity('alex')
+
+
+CREATE function GetTheDomainByStudentID (@id int)
+returns varchar(50)
+as
+begin
+return(
+	select SUBSTRING (email, CHARINDEX( '@', [email]) + 1,LEN([email])) AS 'Domain'
+	from student
+	where student.id = @id
+)
+end
+print dbo.GetTheDomainByStudentID(1)
